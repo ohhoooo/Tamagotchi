083f2f715252b69699c03882bd39fb18d2af053c
KJH <gh7052@gmail.com>
2025-02-23 02:27:12 +0900

    feat: TamagotchiSelectionViewModel 구현

--- Tamagotchi/ViewModel/TamagotchiSelectionViewModel.swift
+++ Tamagotchi/ViewModel/TamagotchiSelectionViewModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class TamagotchiSelectionViewModel: BaseViewModel {
+    
+    // MARK: - properties
+    struct Input {
+        let modelSelected: ControlEvent<Tamagotchi>
+    }
+    
+    struct Output {
+        let items: Observable<[Tamagotchi]>
+        let modelSelected: ControlEvent<Tamagotchi>
+    }
+    
+    // MARK: - methods
+    func transform(input: Input) -> Output {
+        let items = Observable.just(fetchTamagotchi())
+        
+        return Output(
+            items: items,
+            modelSelected: input.modelSelected
+        )
+    }
+    
+    private func fetchTamagotchi() -> [Tamagotchi] {
+        guard let data = loadData() else { return [] }
+        
+        do {
+            return try JSONDecoder().decode([Tamagotchi].self, from: data)
+        } catch {
+            return []
+        }
+    }
+    
+    private func loadData() -> Data? {
+        guard let url = Bundle.main.url(forResource: "Tamagotchi", withExtension: "json") else { return nil }
+        
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            return nil
+        }
+    }
+}
