ee791604d145b61c95b7ed0a79e0074f4b5e7574
KJH <gh7052@gmail.com>
2025-02-22 18:44:03 +0900

    feat: UIColor+Extension 구현

---
+++ Tamagotchi/Util/Extension/UIColor+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extension.swift
+//  Tamagotchi
+//
+//  Created by 김정호 on 2/22/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static var primaryColor: UIColor {
+        return UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+    }
+    
+    static var primaryBackgroundColor: UIColor {
+        return UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    }
+}
