20b2507580f153ee98d252df780f7201bad07d52
KJH <gh7052@gmail.com>
2025-02-24 07:39:03 +0900

    feat: SettingNicknameViewModel 데이터 바인딩 및 이름 변경 로직 구현

--- Tamagotchi/ViewModel/SettingNicknameViewModel.swift
+++ Tamagotchi/ViewModel/SettingNicknameViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class SettingNicknameViewModel: BaseViewModel {
+    
+    // MARK: - properties
+    struct Input {
+        let changedTextFieldText: ControlEvent<String?>
+        let tapSaveBarButtonItem: ControlEvent<Void>
+    }
+    
+    struct Output {
+        let nickname: Observable<String>
+        let saveCompletion: PublishRelay<Void>
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    private var nicknameText = ""
+    
+    // MARK: - methods
+    func transform(input: Input) -> Output {
+        let nickname = Observable.just(UserDefaultsManager.nickname)
+        let saveCompletion = PublishRelay<Void>()
+        
+        input.changedTextFieldText
+            .bind(with: self) { owner, value in
+                owner.nicknameText = value ?? ""
+            }
+            .disposed(by: disposeBag)
+        
+        input.tapSaveBarButtonItem
+            .bind(with: self) { owner, _ in
+                if owner.nicknameText.count >= 2 && owner.nicknameText.count <= 6 {
+                    UserDefaultsManager.nickname = owner.nicknameText
+                    saveCompletion.accept(())
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            nickname: nickname,
+            saveCompletion: saveCompletion
+        )
+    }
+}
