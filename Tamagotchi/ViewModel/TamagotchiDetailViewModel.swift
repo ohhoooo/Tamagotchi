0efa6c783f609fb1ea61ed6a5d82241a48f4a4e1
KJH <gh7052@gmail.com>
2025-02-23 23:58:31 +0900

    feat: TamagotchiDetailViewModel 구현

--- Tamagotchi/ViewModel/TamagotchiDetailViewModel.swift
+++ Tamagotchi/ViewModel/TamagotchiDetailViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class TamagotchiDetailViewModel: BaseViewModel {
+    
+    // MARK: - properties
+    struct Input {
+        let tapCancelButton: ControlEvent<Void>
+        let tapStartButton: ControlEvent<Void>
+    }
+    
+    struct Output {
+        let tamagotchi: Observable<Tamagotchi>
+        let tapCancelButton: ControlEvent<Void>
+        let tapStartButton: ControlEvent<Void>
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    private let tamagotchi: Tamagotchi
+    
+    // MARK: - life cycles
+    init(tamagotchi: Tamagotchi) {
+        self.tamagotchi = tamagotchi
+    }
+    
+    // MARK: - methods
+    func transform(input: Input) -> Output {
+        let tamagotchi = Observable.just(tamagotchi)
+        
+        input.tapStartButton.bind { _ in
+            guard let tamagotchis = UserDefaultsManager.tamagotchis else { return }
+            
+            for (index, element) in tamagotchis.enumerated() {
+                UserDefaultsManager.tamagotchis?[index].isSelected = element.nickname == self.tamagotchi.nickname ? true : false
+            }
+            
+            UserDefaultsManager.isSelected = true
+        }
+        .disposed(by: disposeBag)
+        
+        return Output(
+            tamagotchi: tamagotchi,
+            tapCancelButton: input.tapCancelButton,
+            tapStartButton: input.tapStartButton
+        )
+    }
+}
