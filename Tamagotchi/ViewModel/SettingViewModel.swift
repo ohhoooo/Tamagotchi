add83377bc8689fb42d25e32dd839c566d8e96e7
KJH <gh7052@gmail.com>
2025-02-24 05:52:01 +0900

    feat: SettingViewModel 데이터 바인딩, 데이터 초기화 기능 구현

--- Tamagotchi/ViewModel/SettingViewModel.swift
+++ Tamagotchi/ViewModel/SettingViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class SettingViewModel: BaseViewModel {
+    
+    // MARK: - properties
+    struct Input {
+        let itemSelected: ControlEvent<IndexPath>
+    }
+    
+    struct Output {
+        let settings: Observable<[Setting]>
+        let itemSelected: ControlEvent<IndexPath>
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    let tapOkButton = PublishRelay<Void>()
+    
+    // MARK: - methods
+    func transform(input: Input) -> Output {
+        let settings = Observable.just(fetchSettings())
+        
+        tapOkButton
+            .bind {
+                UserDefaultsManager.isSelected = false
+                UserDefaultsManager.tamagotchis = nil
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            settings: settings,
+            itemSelected: input.itemSelected
+        )
+    }
+    
+    private func fetchSettings() -> [Setting] {
+        guard let data = loadData() else { return [] }
+        
+        do {
+            return try JSONDecoder().decode([Setting].self, from: data)
+        } catch {
+            return []
+        }
+    }
+    
+    private func loadData() -> Data? {
+        guard let url = Bundle.main.url(forResource: "Setting", withExtension: "json") else { return nil }
+        
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            return nil
+        }
+    }
+}
