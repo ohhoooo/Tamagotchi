5c50e28842d16222555ed7b5c0fcb714ce57ebc2
KJH <gh7052@gmail.com>
2025-02-24 16:19:03 +0900

    feat: SettingViewController 데이터 바인딩 및 cell 선택 시 조건에 따른 화면 이동 기능 구현

--- Tamagotchi/View/Setting/SettingViewController.swift
+++ Tamagotchi/View/Setting/SettingViewController.swift
@@ -6,24 +6,81 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class SettingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class SettingViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let settingView = SettingView()
+    private let viewModel = SettingViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = settingView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureNavigation()
     }
-    */
-
+    
+    // MARK: - methods
+    override func bind() {
+        let input = SettingViewModel.Input(
+            itemSelected: settingView.tableView.rx.itemSelected
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        output.settings
+            .bind(to: settingView.tableView.rx.items(
+                cellIdentifier: SettingCell.identifier,
+                cellType: SettingCell.self)
+            ) { (row, element, cell) in
+                cell.bind(setting: element)
+            }
+            .disposed(by: disposeBag)
+        
+        output.itemSelected
+            .bind(with: self) { owner, indexPath in
+                switch indexPath.row {
+                case 0:
+                    owner.navigationController?.pushViewController(SettingNicknameViewController(), animated: true)
+                case 1:
+                    owner.navigationController?.pushViewController(SettingTamagotchiViewController(), animated: true)
+                default:
+                    owner.showAlert()
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "설정"
+        navigationController?.navigationBar.topItem?.title = ""
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: "데이터 초기화",
+                                      message: "정말 다시 처음부터 시작하실 건가용?",
+                                      preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "확인", style: .default) { [weak self] _ in
+            self?.viewModel.tapOkButton.accept(())
+            
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first else { return }
+            window.rootViewController = UINavigationController(rootViewController: TamagotchiSelectionViewController())
+            window.makeKeyAndVisible()
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        present(alert, animated: true)
+    }
 }
