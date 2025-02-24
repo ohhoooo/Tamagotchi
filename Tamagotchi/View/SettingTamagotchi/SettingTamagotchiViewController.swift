16326f3d822cb80f82f74c2ac601f586de14308a
KJH <gh7052@gmail.com>
2025-02-24 15:40:03 +0900

    feat: SettingTamagotchiViewController 데이터 바인딩 및 cell 선택 시 조건에 따른 화면 이동 기능 구현

---
+++ Tamagotchi/View/SettingTamagotchi/SettingTamagotchiViewController.swift
@@ -0,0 +1,66 @@
+//
+//  SettingTamagotchiViewController.swift
+//  Tamagotchi
+//
+//  Created by 김정호 on 2/24/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class SettingTamagotchiViewController: BaseViewController {
+    
+    // MARK: - properties
+    private let tamagotchiSelectionView = TamagotchiSelectionView()
+    private let viewModel = TamagotchiSelectionViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = tamagotchiSelectionView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureNavigation()
+    }
+    
+    // MARK: - methods
+    override func bind() {
+        let input = TamagotchiSelectionViewModel.Input(
+            modelSelected: tamagotchiSelectionView.collectionView.rx.modelSelected(Tamagotchi.self)
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        output.items
+            .bind(to: tamagotchiSelectionView.collectionView.rx.items(
+                cellIdentifier: TamagotchiSelectionCell.identifier,
+                cellType: TamagotchiSelectionCell.self)
+            ) { (row, element, cell) in
+                cell.bind(tamagotchi: element)
+            }
+            .disposed(by: disposeBag)
+        
+        output.modelSelected
+            .bind(with: self) { owner, value in
+                if value.nickname != "준비중이에요" {
+                    let tamagotchiDetailVM = TamagotchiDetailViewModel(tamagotchi: value)
+                    let tamagotchiDetailVC = TamagotchiDetailViewController(viewModel: tamagotchiDetailVM)
+                    
+                    tamagotchiDetailVC.modalTransitionStyle = .crossDissolve
+                    tamagotchiDetailVC.modalPresentationStyle = .overCurrentContext
+                    
+                    owner.present(tamagotchiDetailVC, animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "다마고치 변경하기"
+    }
+}
